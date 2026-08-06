bdeeb2ae3900f475f76f60b078df1904100a6f8b
Fedoseev Matvei <165332361+Depak00@users.noreply.github.com>
2026-08-06 03:45:53 +0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-yay -S sway waybar mako kanshi kitty thunar firefox polkit-gnome autotiling wl-clipboard-persist pavucontrol brightnessctl blueberry wofi swaylock swaybg --needed &> /dev/null
-mv {waybar,mako,kanshi,sway,wofi} ~/.config/
+yay -S sway waybar mako kanshi kitty thunar firefox polkit-gnome autotiling wl-clip-persist xorg-xwayland pavucontrol brightnessctl blueberry wofi swaylock swaybg nmrs --needed --noconfirm &> /dev/null
+cp {waybar,mako,kanshi,sway,wofi} ~/.config/
